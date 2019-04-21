4e42710855277f94c9438d82539be8f6723a2082
Cody <cody.tracy92@gmail.com>
2019-04-21 11:11:01 -0500

    got the logic for figuring out which friend is the closest match working in the console

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,4 @@
 var friends = require("../data/friends.js")
-var friendsData = require("../data/friends")
 
 
 module.exports = function(app){
@@ -8,7 +7,26 @@ module.exports = function(app){
     });
 
     app.post("/api/friends",function(req,res){
+        var friendsArr = friends;
+        var scoresComp = [];
         friends.push(req.body);
+        for(var i = 0; i < (friendsArr.length - 1); i++){
+            var difference = 0;
+            for(var a = 0; a < 10; a++){
+            var userNum = parseInt(req.body.scores[a])
+            var friendNum = parseInt(friendsArr[i].scores[a])
+            difference+= Math.abs(userNum - friendNum);
+            }
+            scoresComp.push(difference);
+
+            console.log(friendsArr[i].scores,i,friendsArr[i].name)
+        }
+        for(var d = 0; d<scoresComp.length; d++){
+            if (scoresComp[d] === Math.min(...scoresComp))[
+                console.log(friendsArr[d],"THIS IS YOUR NEW FRIEND")
+            ]
+        }
+        console.log(scoresComp,"SCORES");
     })
 }
 
